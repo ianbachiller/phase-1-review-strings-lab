b60c8b78eb4ea52375898ff002dcdbb2c949e544
Ian Bachiller <ianbachise@gmail.com>
2024-01-31 04:12:39 -0500

    Complete review strings lab

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+
+const currentUser = "string";
+const welcomeMessage = `Welcome to Flatbook, ` + currentUser + "!";
+const excitedWelcomeMessage = "WELCOME TO FLATBOOK, STRING!";
+const shortGreeting = "Welcome, " + currentUser[0] + "!";
